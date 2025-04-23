fa085cfd0805869e83e92ac62ed34d9526739b88
D.EUN <77758058+Be-Learning@users.noreply.github.com>
2025-04-23 11:10:12 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Confused)
+})
 basic.forever(function () {
-	
+    basic.showString("Rkakdenddl")
+    basic.pause(1000)
+    basic.showNumber(1004)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
+    basic.pause(500)
 })
